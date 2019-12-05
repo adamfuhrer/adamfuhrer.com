17fcd47fa6a4b27164f30cc046dfc8a0fb37795f
adamfuhrer <adam.fuhrer@caseware.com>
2019-12-04 21:33:58 -0500

    add button to swing card

--- src/app/components/homepage/homepage.component.ts
+++ src/app/components/homepage/homepage.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, HostBinding, OnInit} from '@angular/core';
 import {NavigationService} from '../../services/navigation.service';
 import {Title} from '@angular/platform-browser';
 
@@ -8,6 +8,8 @@ import {Title} from '@angular/platform-browser';
     styleUrls: ['./homepage.component.scss']
 })
 export class HomepageComponent implements OnInit {
+    @HostBinding('class.is-swinging') isSwinging = false;
+
     constructor(private navigationService: NavigationService,
                 private titleService: Title) {
     }
@@ -17,5 +19,13 @@ export class HomepageComponent implements OnInit {
 
         // Don't show breadcrumbs on homepage
         this.navigationService.emitBreadcrumbItems([]);
+    }
+
+    onGhostClick() {
+        this.isSwinging = true;
+
+        setTimeout(() => {
+            this.isSwinging = false;
+        }, 4000);
     }
 }
