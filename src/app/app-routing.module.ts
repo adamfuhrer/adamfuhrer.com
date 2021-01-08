0251fd803585f9631b7b76b79971ca04a6df9a47
adamfuhrer <adamfuhrer@gmail.com>
2021-01-07 23:49:06 -0500

    2021 update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,17 +13,18 @@ import {ColorsComponent} from './components/projects/colors/colors.component';
 
 const routes: Routes = [
     {path: '', component: HomepageComponent},
-    {path: 'accuracy-of-mechanical-watches', component: MechanicalWatchesAccuracyComponent},
-    {path: 'watches', redirectTo: 'accuracy-of-mechanical-watches'},
-    {path: 'accuracy-of-mechanical-watches/seiko-5-sports', component: SeikoFiveComponent},
-    {path: 'accuracy-of-mechanical-watches/rolex-submariner', component: RolexSubmarinerComponent},
-    {path: 'the-curated-feed', component: TheCuratedFeedComponent},
+    // {path: 'accuracy-of-mechanical-watches', component: MechanicalWatchesAccuracyComponent},
+    // {path: 'watches', redirectTo: 'accuracy-of-mechanical-watches'},
+    // {path: 'accuracy-of-mechanical-watches/seiko-5-sports', component: SeikoFiveComponent},
+    // {path: 'accuracy-of-mechanical-watches/rolex-submariner', component: RolexSubmarinerComponent},
+    // {path: 'the-curated-feed', component: TheCuratedFeedComponent},
+    {path: 'pantone-colors', redirectTo: 'visualizing-every-pantone-color-of-the-year'},
     {path: 'visualizing-every-pantone-color-of-the-year', component: ColorOfTheYearComponent},
-    {path: '28-portraits-by-28-photographers', component: PortraitsComponent},
-    {path: 'glitch-art-generator', component: GlitchArtComponent},
-    {path: 'css-mondrian', component: CssMondrianComponent},
-    {path: 'colors-lol', component: ColorsComponent},
-    {path: 'portraits', redirectTo: '28-portraits-by-28-photographers'},
+    // {path: '28-portraits-by-28-photographers', component: PortraitsComponent},
+    // {path: 'glitch-art-generator', component: GlitchArtComponent},
+    // {path: 'css-mondrian', component: CssMondrianComponent},
+    // {path: 'colors-lol', component: ColorsComponent},
+    // {path: 'portraits', redirectTo: '28-portraits-by-28-photographers'},
     {path: '**', redirectTo: '', component: HomepageComponent},
 ];
 
--- src/app/components/homepage/homepage.component.ts
+++ src/app/components/homepage/homepage.component.ts
@@ -1,4 +1,4 @@
-import {Component, HostBinding, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {NavigationService} from '../../services/navigation.service';
 import {Title} from '@angular/platform-browser';
 
@@ -8,24 +8,14 @@ import {Title} from '@angular/platform-browser';
     styleUrls: ['./homepage.component.scss']
 })
 export class HomepageComponent implements OnInit {
-    @HostBinding('class.is-swinging') isSwinging = false;
-
     constructor(private navigationService: NavigationService,
                 private titleService: Title) {
     }
 
     ngOnInit() {
-        this.titleService.setTitle('Adam Fuhrer | UX Developer');
+        this.titleService.setTitle('Adam Fuhrer | Software Developer');
 
         // Don't show breadcrumbs on homepage
         this.navigationService.emitBreadcrumbItems([]);
-    }
-
-    onGhostClick() {
-        this.isSwinging = true;
-
-        setTimeout(() => {
-            this.isSwinging = false;
-        }, 4000);
     }
 }
